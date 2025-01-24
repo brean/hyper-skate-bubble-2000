ba1e044700c05cce3c4e9faeb8ba2aa19cf06adb
lopaker | Johannes <lopaker@arentia.de>
2025-01-25 00:32:46 +0100

    fixed main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -12,8 +12,8 @@ func _process(delta: float) -> void:
 
 # Function to close the game
 func close_game():
-	get_cancel_button().pressed.connect(_on_canceled)
-
+	#get_cancel_button().pressed.connect(_on_canceled)
+	pass
 func _on_canceled():
 	print("Closing the game...")
 	get_tree().quit()
