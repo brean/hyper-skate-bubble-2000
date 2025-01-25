c6693a78ff8f4080e6bd53b79c00e6c457ce9870
lopaker | Johannes <lopaker@arentia.de>
2025-01-25 19:40:57 +0100

    edit input for selecting button

--- models/bubble.gd
+++ models/bubble.gd
@@ -15,55 +15,55 @@ extends CharacterBody3D
 
 
 func _ready():
-    Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 func _physics_process(_delta):
-    var gravity = ProjectSettings.get_setting("physics/3d/default_gravity_vector")
+	var gravity = ProjectSettings.get_setting("physics/3d/default_gravity_vector")
 
-    if !is_on_floor():
-        velocity.y += gravity.y * jump_mass
-        move_and_slide()
-        return
+	if !is_on_floor():
+		velocity.y += gravity.y * jump_mass
+		move_and_slide()
+		return
 
-    if !Input.is_anything_pressed():
-        velocity = gravity
-        move_and_slide()
-        return
+	if !Input.is_anything_pressed():
+		velocity = gravity
+		move_and_slide()
+		return
 
-    if Input.is_action_pressed("jump") and is_on_floor():
-        print("jump")
-        velocity.y += jump_force
-        move_and_slide()
-        return
+	if Input.is_action_pressed("jump") and is_on_floor():
+		print("jump")
+		velocity.y += jump_force
+		move_and_slide()
+		return
 
-    if Input.is_action_pressed("look_left"):
-        rotation_degrees.y += rotation_delta * sensitivity * Input.get_action_strength("look_left")
-    elif Input.is_action_pressed("look_right"):
-        rotation_degrees.y -= rotation_delta * sensitivity * Input.get_action_strength("look_right")
+	if Input.is_action_pressed("look_left"):
+		rotation_degrees.y += rotation_delta * sensitivity * Input.get_action_strength("look_left")
+	elif Input.is_action_pressed("look_right"):
+		rotation_degrees.y -= rotation_delta * sensitivity * Input.get_action_strength("look_right")
 
-    var movement_delta = movement_speed * sensitivity
-    var local_movement = to_local(Vector3.ZERO)
-    if Input.is_action_pressed("move_right"):
-        movement_delta = movement_delta * Input.get_action_strength("move_right")
-        local_movement += Vector3.LEFT * movement_delta
-    if Input.is_action_pressed("move_left"):
-        movement_delta = movement_delta * Input.get_action_strength("move_left")
-        local_movement += Vector3.RIGHT * movement_delta
-    if Input.is_action_pressed("move_forwards"):
-        movement_delta = movement_delta * Input.get_action_strength("move_forwards")
-        local_movement += Vector3.BACK * movement_delta
-    if Input.is_action_pressed("move_backwards"):
-        movement_delta = movement_delta * Input.get_action_strength("move_backwards")
-        local_movement += Vector3.FORWARD * movement_delta
-    velocity = to_global(local_movement)
-    velocity.y = gravity.y
-    $Mesh.rotation += local_movement
-    move_and_slide()
+	var movement_delta = movement_speed * sensitivity
+	var local_movement = to_local(Vector3.ZERO)
+	if Input.is_action_pressed("move_right"):
+		movement_delta = movement_delta * Input.get_action_strength("move_right")
+		local_movement += Vector3.LEFT * movement_delta
+	if Input.is_action_pressed("move_left"):
+		movement_delta = movement_delta * Input.get_action_strength("move_left")
+		local_movement += Vector3.RIGHT * movement_delta
+	if Input.is_action_pressed("move_forwards"):
+		movement_delta = movement_delta * Input.get_action_strength("move_forwards")
+		local_movement += Vector3.BACK * movement_delta
+	if Input.is_action_pressed("move_backwards"):
+		movement_delta = movement_delta * Input.get_action_strength("move_backwards")
+		local_movement += Vector3.FORWARD * movement_delta
+	velocity = to_global(local_movement)
+	velocity.y = gravity.y
+	$Mesh.rotation += local_movement
+	move_and_slide()
 
 
 func _input(event: InputEvent) -> void:
-    if event is InputEventMouseMotion:
-        var x_velocity = snapped(event.screen_velocity.x, 1)
-        if abs(x_velocity) > camera_deadzone:
-            rotation_degrees.y += (-x_velocity / rotation_divisor) * sensitivity
+	if event is InputEventMouseMotion:
+		var x_velocity = snapped(event.screen_velocity.x, 1)
+		if abs(x_velocity) > camera_deadzone:
+			rotation_degrees.y += (-x_velocity / rotation_divisor) * sensitivity
