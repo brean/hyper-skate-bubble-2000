e07598387d3e46cf89c5a722cc46cf18a7fe4cc8
Andreas Bresser <self@andreasbresser.de>
2025-01-25 20:15:59 +0100

    die when touching spikes

--- scripts/audio_fader.gd
+++ scripts/audio_fader.gd
@@ -6,8 +6,8 @@ extends Node
 
 # Function to fade out the music
 func fade_out_music():
-	var tween = get_tree().create_tween()
-	# Ensure the music player is playing before starting the fade
-	if music_player.is_playing():
-		# Fade the volume out over time
-		tween.tween_property(music_player, "volume_db", -80.0, fade_out_time)
+    var tween = get_tree().create_tween()
+    # Ensure the music player is playing before starting the fade
+    if music_player.is_playing():
+        # Fade the volume out over time
+        tween.tween_property(music_player, "volume_db", -80.0, fade_out_time)
--- scripts/generate_level.gd
+++ scripts/generate_level.gd
@@ -8,19 +8,19 @@ var corner = preload("res://scenes/skate_corner.tscn")
 var tiles = 100
 
 func next_tile():
-	var r = randf()
-	if r < 0.7:
-		return straight.instantiate()
-	var next = corner.instantiate()
-	if r < 0.85:
-		return next
-	#next.transform.rotation_degrees.z = 90
-	return next
+    var r = randf()
+    if r < 0.7:
+        return straight.instantiate()
+    var next = corner.instantiate()
+    if r < 0.85:
+        return next
+    #next.transform.rotation_degrees.z = 90
+    return next
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	seed(12)
-	for tile_no in range(tiles):
-		var tile = next_tile()
-		tile.position = Vector3(tile_no * 10, 0, 0)
-		add_child(tile)
+    seed(12)
+    for tile_no in range(tiles):
+        var tile = next_tile()
+        tile.position = Vector3(tile_no * 10, 0, 0)
+        add_child(tile)
